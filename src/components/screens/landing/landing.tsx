aea50045521b4e6619935c99b93f5ff07d86713f
FreshEffect15 <142315233+FreshEffect15@users.noreply.github.com>
2025-05-06 15:37:10 -0300

    Topbar Agregada

--- src/components/screens/landing/landing.tsx
+++ src/components/screens/landing/landing.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import Footer from '../../ui/footer/footer'; 
+import Topbar from '../../ui/topbar/topbar';
+import Footer from '../../ui/footer/footer';
 
 const Landing = () => {
   return (
     <div>
+      <Topbar />
+      
       <div style={{ minHeight: '80vh' }}>
-        <h1 style={{ textAlign: 'center', padding: '2rem' }}>Landing provisional, rellenar aca</h1>
+        <h1 style={{ textAlign: 'center', padding: '2rem' }}>
+          Landing provisional, rellenar aca
+        </h1>
       </div>
 
       <Footer />
