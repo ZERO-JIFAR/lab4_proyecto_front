22330987f1992dbfe679c96ac065fafbd89da587
ZERO-JIFAR <juanignacioarnold@gmail.com>
2025-05-25 23:56:19 -0300

    otro componente de editar

--- src/components/ui/topbar/modals/adminMenu.tsx
+++ src/components/ui/topbar/modals/adminMenu.tsx
@@ -1,25 +1,30 @@
 import React, { useState } from 'react';
 import styles from './adminMenu.module.css';
 import ModalAddProd from './modalAddProd';
+import ModalEditProd from './modalEditProd';
 
 interface AdminMenuProps {
   visible: boolean;
 }
 
 const AdminMenu: React.FC<AdminMenuProps> = ({ visible }) => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalAdd, setModalAdd] = useState(false);
+  const [modalEdit, setModalEdit] = useState(false);
 
   if (!visible) return null;
 
   return (
     <div className={styles.menuContainer}>
-      <button className={styles.menuButton} onClick={() => setIsModalOpen(true)}>
+      <button className={styles.menuButton} onClick={() => setModalAdd(true)}>
         Agregar Producto
       </button>
-      <button className={styles.menuButton}>Editar Producto</button>
+      <button className={styles.menuButton} onClick={() => setModalEdit(true)}>
+        Editar Producto
+      </button>
       <button className={styles.menuButton}>Ver Estadísticas</button>
 
-      <ModalAddProd isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
+      <ModalAddProd isOpen={modalAdd} onClose={() => setModalAdd(false)} />
+      <ModalEditProd isOpen={modalEdit} onClose={() => setModalEdit(false)} />
     </div>
   );
 };
