310a14f31d448122839d143fadc38c7261d3e2dd
ZERO-JIFAR <juanignacioarnold@gmail.com>
2025-06-09 20:07:13 -0300

    mas ofertas eliminadas

--- src/components/ui/cardProduct/cardProduct.tsx
+++ src/components/ui/cardProduct/cardProduct.tsx
@@ -4,23 +4,18 @@ import styles from './cardProduct.module.css';
 interface CardProductProps {
     title: string;
     price: number;
-    oldPrice?: number; // si hay precio anterior, mostramos "OFERTA"
     image: string;
 }
 
-const CardProduct: React.FC<CardProductProps> = ({ title, price, oldPrice, image }) => {
-    const hasDiscount = oldPrice && oldPrice > price;
-
+const CardProduct: React.FC<CardProductProps> = ({ title, price, image }) => {
     return (
         <div className={styles.card}>
             <div className={styles.imageContainer}>
-                {hasDiscount && <span className={styles.offerBadge}>OFERTA</span>}
                 <img src={image} alt={title} className={styles.image} />
             </div>
             <div className={styles.content}>
                 <p className={styles.title}>{title}</p>
                 <p className={styles.price}>
-                {hasDiscount && <span className={styles.oldPrice}>${oldPrice}</span>}
                 <span>${price}</span>
                 </p>
             </div>
