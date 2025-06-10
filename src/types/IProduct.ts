d5536f2a02030c991b848dc80bd3750f64d67ecf
FreshEffect15 <gossuinulises@gmail.com>
2025-06-10 16:49:43 -0300

    types de productos y categorias

---
+++ src/types/IProduct.ts
@@ -0,0 +1,13 @@
+import { ICategory } from "./ICategory";
+
+export interface IProduct {
+  id: number;
+  nombre: string;
+  cantidad: number;
+  precio: number;
+  descripcion?: string;
+  color?: string;
+  marca?: string;
+  eliminado: boolean;
+  categoria: ICategory;
+}
