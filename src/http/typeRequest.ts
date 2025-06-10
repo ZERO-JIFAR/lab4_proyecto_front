962f585cc60a54c6ce4771d02ef137e869bdab96
ZERO-JIFAR <juanignacioarnold@gmail.com>
2025-06-10 20:31:39 -0300

    modificacion del link

--- src/http/typeRequest.ts
+++ src/http/typeRequest.ts
@@ -1,33 +1,32 @@
 import axios from "axios";
 import { ITipo } from "../types/IType";
 
-
-const API_URL = "http://localhost:9000/api/tipos";
-
+const APIURL = import.meta.env.VITE_API_URL;
+const baseURL = `${APIURL}/tipos`;
 
 export const getTipos = async (): Promise<ITipo[]> => {
-  const response = await axios.get<ITipo[]>(API_URL);
+  const response = await axios.get<ITipo[]>(baseURL);
   return response.data;
 };
 
 
 export const getTipoById = async (id: number): Promise<ITipo | null> => {
-  const response = await axios.get<ITipo>(`${API_URL}/${id}`);
+  const response = await axios.get<ITipo>(`${baseURL}/${id}`);
   return response.data;
 };
 
 
 export const createTipo = async (tipo: Omit<ITipo, "id">): Promise<ITipo> => {
-  const response = await axios.post<ITipo>(API_URL, tipo);
+  const response = await axios.post<ITipo>(baseURL, tipo);
   return response.data;
 };
 
 
 export const updateTipo = async (id: number, tipo: Omit<ITipo, "id">): Promise<ITipo> => {
-  const response = await axios.put<ITipo>(`${API_URL}/${id}`, tipo);
+  const response = await axios.put<ITipo>(`${baseURL}/${id}`, tipo);
   return response.data;
 };
 
 export const deleteTipo = async (id: number): Promise<void> => {
-  await axios.delete(`${API_URL}/${id}`);
+  await axios.delete(`${baseURL}/${id}`);
 };
