847dcdd0f3709bd5a10b8a370453e617a1a7d6de
ZERO-JIFAR <juanignacioarnold@gmail.com>
2025-06-15 17:14:58 -0300

    mejorado el footer

--- src/components/ui/footer/footer.tsx
+++ src/components/ui/footer/footer.tsx
@@ -6,27 +6,43 @@ import ModalFooter from './modalsFooter/modalFooter';
 const infoMap: Record<string, { title: string; content: string }> = {
   Envíos: {
     title: 'Información sobre Envíos',
-    content: 'Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.',
+    content: 'Realizamos envíos a todo el país. Una vez confirmado tu pedido, el tiempo de entrega estimado es de 3 a 7 días hábiles, dependiendo de tu ubicación. Recibirás un correo con el seguimiento para que puedas ver el estado de tu compra en todo momento.'
   },
   Devoluciones: {
     title: 'Política de Devoluciones',
-    content: 'Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.',
+    content: 'Si no estás conforme con tu compra, podés solicitar una devolución dentro de los 10 días posteriores a la recepción del producto. Los artículos deben estar sin uso, en su empaque original y con etiquetas. Consultá nuestras condiciones para más detalles.'
   },
   Cambios: {
     title: 'Cambios de Productos',
-    content: 'Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.',
+    content: '¿Necesitás cambiar un talle o modelo? Tenés hasta 15 días desde que recibís tu compra para hacerlo. El producto debe estar en perfecto estado, con sus etiquetas y sin uso. Contactanos para coordinar el cambio.'
   },
   Contacto: {
     title: 'Contacto',
-    content: 'Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.',
+    content: '¿Tenés dudas o necesitás ayuda? Escribinos por Twitter o Instagram. Nuestro equipo está disponible de lunes a viernes de 9 a 18 hs.'
   },
   Tips: {
     title: 'Consejos y Tips',
-    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Pellentesque habitant morbi tristique senectus et netus.',
+    content: 'Te compartimos ideas para combinar tus prendas, cuidar tus textiles y estar al día con las últimas tendencias. ¡Sacale el máximo provecho a tu estilo!'
   },
   Salud: {
     title: 'Salud y Bienestar',
-    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut perspiciatis unde omnis iste natus error sit voluptatem.',
+    content: 'Creemos que la moda también es sentirse bien. Por eso, te acercamos contenidos sobre hábitos saludables, autocuidado y bienestar emocional.'
+  },
+    Promociones: {
+    title: 'Promociones',
+    content: 'Aprovechá nuestras ofertas especiales y descuentos exclusivos. Suscribite a nuestro newsletter para enterarte antes que nadie de las próximas promos.'
+  },
+    Explorá: {
+    title: 'Encontrá tus artículos',
+    content: 'Explorá nuestras categorías y descubrí la prenda perfecta para vos. Usá los filtros para buscar por talle, color o tipo de producto y hacé tu compra en minutos.'
+  },
+    Nosotros: {
+    title: '¿Quiénes somos?',
+    content: 'Somos una marca argentina dedicada a la moda urbana y actual. Trabajamos con pasión para ofrecerte calidad, estilo y comodidad en cada prenda.'
+  },
+    Noticias: {
+    title: 'Noticias',
+    content: 'Enterate de nuestras novedades, lanzamientos, colaboraciones y eventos. Seguinos en redes para estar siempre actualizado con lo último de nuestra marca.'
   },
 };
 
@@ -69,17 +85,16 @@ const Footer = () => {
           <div className={styles.footerColumn}>
             <h4 className={styles.footerTitle}>ACERCA DE</h4>
             <ul className={styles.footerList}>
-              <li className={styles.footerListItem}>¿Quiénes somos?</li>
-              <li className={styles.footerListItem}>Noticias</li>
+              {['Nosotros','Noticias'].map((key) => (
+                <li key={key} className={styles.footerListItem} onClick={() => handleOpenModal(key)}>{key}</li>
+              ))}
             </ul>
           </div>
 
           <div className={styles.footerColumn}>
             <h4 className={styles.footerTitle}>Novedades</h4>
             <ul className={styles.footerList}>
-              <li className={styles.footerListItem}>Promociones</li>
-              <li className={styles.footerListItem}>Encontrá tus artículos</li>
-              {['Tips', 'Salud'].map((key) => (
+              {['Promociones','Explorá','Tips', 'Salud'].map((key) => (
                 <li key={key} className={styles.footerListItem} onClick={() => handleOpenModal(key)}>{key}</li>
               ))}
             </ul>
