5776dad2469c55d95b006398b57be271d955d503
ZERO-JIFAR <juanignacioarnold@gmail.com>
2025-06-26 19:24:37 -0300

    cambiado texto

--- src/components/screens/landing/landing.tsx
+++ src/components/screens/landing/landing.tsx
@@ -135,7 +135,7 @@ const Landing = () => {
 
         <div className={styles.landingButtonWrapper}>
           <Link to="/SearchItem">
-            <button className={styles.landingBuyButton}>¡Compre Ahora!</button>
+            <button className={styles.landingBuyButton}>Todos los productos</button>
           </Link>
         </div>
 
